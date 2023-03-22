e3b42f3fda398011c5f979f8e4966d58271b9ffd
Masih <masih.saldanha@gmail.com>
2023-03-22 20:46:37 -0300

    feat: finish the project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] array_1 = new String[]{"a", "b", "c"};
+        String[] array_2 = new String[]{"b", "c", "d"};
+        List<String> array_3 = new ArrayList<>();
+
+        for (int i = 0; i < array_1.length; i++) {
+            String element_1 = array_1[i];
+            for (int j = 0; j < array_2.length; j++) {
+                String element_2 = array_2[j];
+                if (element_1 == element_2) {
+                    array_3.add(element_2);
+                }
+            }
+        }
+
+        System.out.println(array_3);
     }
 }
